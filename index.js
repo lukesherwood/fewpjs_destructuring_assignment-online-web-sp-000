308ee64cefe07a66cc7abdb384f3c718c95df651
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-09 01:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,4 +49,4 @@ const [, , , , , indg, ] = colors
 const { name, color, song, job, partner } = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
-const { job, partner } = k_muppet.k_album.the_muppet_movie[]
+const { job, partner } = k_muppet.k_album.the_muppet_movie[song_2, song_4]
